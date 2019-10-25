de99f71ea3f00fc241ef70da2fc684159270f7ae
fourianalist <endangfouriana@gmail.com>
2019-10-25 11:06:57 -0500

    Added to Github

---
+++ Exercise2.R
@@ -0,0 +1,39 @@
+
+######################### Data wrangling in R ######################
+
+#### Based on the data carpentry ecology lessons: 
+
+####       http://www.datacarpentry.org/R-ecology-lesson/03-dplyr.html
+
+
+
+#installing packages
+
+#install.packages("tidyverse")
+
+
+
+#loading packages
+
+library("tidyverse")
+
+
+
+#read the data as a data frame
+
+surveys_dot <- read.csv("data/raw_surveys.csv")
+
+surveys_dot
+
+str(surveys_dot)
+
+
+
+#read the data as a 'tibble'
+
+surveys <- read_csv('data/raw_surveys.csv')
+
+surveys
+
+str(surveys)
+
